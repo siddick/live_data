087e88014b475261d1255cc63fa002467ef7ac59
Mohammed Siddick.E <siddick007@yahoo.co.in>
2010-02-16 11:01:40 +0530

    Handle multiple wait request for the single user id in LiveData::ThreadWatch

--- lib/live_data/thread_watch.rb
+++ lib/live_data/thread_watch.rb
@@ -5,9 +5,17 @@ module LiveData
       UserThreads = {}
 
       def self.wait( user_id, time = WaitTime )
+         already_use = false
          Lock.synchronize {
-            UserThreads[user_id] = Thread.current
+            if( UserThreads[user_id] )
+               already_use = true
+            else
+               UserThreads[user_id] = Thread.current
+            end
          }
+         if( already_use )
+            return nil
+         end
          used_time = sleep(time )
          Lock.synchronize {
             UserThreads.delete( user_id )
--- test/thread_watch_test.rb
+++ test/thread_watch_test.rb
@@ -2,13 +2,20 @@ require 'test_helper'
 
 class ThreadWatchTest < Test::Unit::TestCase
 	def test_thread_watch
-		Thread.new{ 
+		th = Thread.new{ 
 			sleep(6)
 			LiveData::ThreadWatch.wakeup('guest')
 		}
 		assert LiveData::ThreadWatch.wait('guest', 10 ) < 10
+		th.join
 
 		assert LiveData::ThreadWatch.wait('guest', 2 ) >= 2 
 
+		th = Thread.new{
+			sleep(2)
+			assert LiveData::ThreadWatch.wait('guest', 2) == nil
+		}
+		assert LiveData::ThreadWatch.wait('guest', 5 ) >= 5
+		th.join
 	end
 end
